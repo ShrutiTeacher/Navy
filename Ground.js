ca296a27cf3379b4b494aea3860d922b00554bec
ShrutiTeacher <73873738+ShrutiTeacher@users.noreply.github.com>
2021-08-22 14:24:24 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,21 @@
+class Ground{
+    constructor(x,y,w,h){
+     var op={
+         "isStatic":true
+     }
+     this.body=Bodies.rectangle(x,y,w,h,op);
+     this.w=w;
+     this.h=h;
+     World.add(world,this.body)
+
+    }
+
+    show(){
+        var pos=this.body.position;
+push();
+noStroke();
+rectMode(CENTER);
+rect(pos.x,pos.y,this.w,this.h)
+pop();
+    }
+}
